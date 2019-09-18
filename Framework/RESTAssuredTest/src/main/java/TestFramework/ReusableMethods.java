c0ff74d96fa7b2cadf703135632cbc6c03aaf116
[SoumalyaDas] <[dassoumalyadas@gmail.com]>
2019-09-18 22:42:00 +0530

    Committing after fixing all the jar related issues

--- Framework/RESTAssuredTest/src/main/java/TestFramework/ReusableMethods.java
+++ Framework/RESTAssuredTest/src/main/java/TestFramework/ReusableMethods.java
@@ -1,9 +1,11 @@
 package TestFramework;
 
 
+import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
 
 public class ReusableMethods {
 	
